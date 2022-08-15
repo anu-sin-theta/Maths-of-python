209f85a648ff9d495deefd87aae713ec640f5501
anubhav <audiq4456@gmail.com>
2022-08-15 16:23:27 +0530

    Initial commit

---
+++ For_loop_addition.py
@@ -0,0 +1,9 @@
+n1 = int(input('Enter first digit:'))
+n2 = int(input('Enter second digit:'))
+x = 0
+for f in range(n1,n2):
+     x+=f
+print(x)
+
+
+
---
+++ f strings.py
@@ -0,0 +1,6 @@
+
+M = 12
+N =13
+print('I have {},{} balls'.format(M,N))
+
+print(f'i have {M},{N} balls')
